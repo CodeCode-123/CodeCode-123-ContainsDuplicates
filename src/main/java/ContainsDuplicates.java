dddd89f08f5b2a06dcbf838f34aad829dbf8147a
Shen Tang <adatang23@yahoo.com>
2025-06-06 15:25:03 +0000

    completed the Contains Duplicates Lab

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.*;;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,13 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int n: nums) {
+            if (set.contains(n)) {
+                return true;
+            }
+            set.add(n);
+        }
         return false;
     }
 }
